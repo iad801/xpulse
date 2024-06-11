6cfa777738906647fb4a720dd1cd07ed1136fb43
iad801 <139395213+iad801@users.noreply.github.com>
2024-06-10 22:03:53 -0400

    Updated Alert

--- js/cosmetics.js
+++ js/cosmetics.js
@@ -3,28 +3,41 @@ function handleWishlistButtonClick(button) {
     var card = button.parentNode;
     var price = parseFloat(card.getAttribute("data-price"));
     var budgetAmount = getBudgetAmountFromCookie();
+    var currentTotal = calculateCurrentTotal();
+    var remainingBudget = budgetAmount - currentTotal;
 
     if (button.classList.contains("added")) {
         // Remove from wishlist
-        budgetAmount += price;
+        remainingBudget += price;
+        currentTotal -= price;
         removeItemFromWishlist(button);
     } else {
         // Add to wishlist
-        if (budgetAmount >= price) {
-            budgetAmount -= price;
+        if (remainingBudget >= price) {
+            remainingBudget -= price;
+            currentTotal += price;
             addItemToWishlist(button);
         } else {
-            showCustomAlert();
+            var overBudgetAmount = price - remainingBudget;
+            showCustomAlert(overBudgetAmount, remainingBudget, currentTotal);
             return;
         }
     }
 
-    updateBudgetAmountInCookie(budgetAmount);
+    updateBudgetAmountInCookie(remainingBudget + currentTotal);
 }
 
 // Function to show custom alert popup
-function showCustomAlert() {
+function showCustomAlert(overBudgetAmount, remainingBudget, currentTotal) {
     var alertPopup = document.getElementById('custom-alert');
+    var alertMessage = document.getElementById('alert-message');
+    var alertBudget = document.getElementById('alert-budget');
+    var alertTotal = document.getElementById('alert-total');
+
+    alertMessage.textContent = `You are ${overBudgetAmount.toFixed(2)} over your budget. Please remove a skin.`;
+    alertBudget.textContent = `Remaining Budget: ${remainingBudget.toFixed(2)}`;
+    alertTotal.textContent = `Current Total: ${currentTotal.toFixed(2)}`;
+
     alertPopup.classList.remove('hidden');
 
     var okButton = document.getElementById('alert-ok-button');
@@ -33,21 +46,31 @@ function showCustomAlert() {
     });
 }
 
+// Function to calculate the current total value of items in the wishlist
+function calculateCurrentTotal() {
+    var total = 0;
+    document.querySelectorAll('.wishlist-button.added').forEach(function(button) {
+        var card = button.parentNode;
+        var price = parseFloat(card.getAttribute("data-price"));
+        total += price;
+    });
+    return total;
+}
+
 // Function to add item to wishlist
 function addItemToWishlist(button) {
     var card = button.parentNode;
     button.classList.add("added");
     button.textContent = "Remove from wishlist";
-    var cardHTML = card.outerHTML;
-    saveCardToCookie(cardHTML);
+    saveCardToCookie(card.outerHTML);
 }
 
 // Function to remove item from wishlist
 function removeItemFromWishlist(button) {
+    var card = button.parentNode;
     button.classList.remove("added");
     button.textContent = "Add to wishlist";
-    // Remove image from cookie
-    removeCardFromCookie();
+    removeCardFromCookie(card.outerHTML);
 }
 
 // Function to update budget amount in cookie
@@ -56,13 +79,18 @@ function updateBudgetAmountInCookie(amount) {
 }
 
 function saveCardToCookie(card) {
-    var cardHTML = card.outerHTML;
-    document.cookie = "wishlistCard=" + encodeURIComponent(cardHTML) + ";path=/";
+    var wishlistCards = getWishlistCardsFromCookie();
+    wishlistCards.push(card);
+    document.cookie = "wishlistCards=" + encodeURIComponent(JSON.stringify(wishlistCards)) + ";path=/";
 }
 
-// Function to remove entire cosmetic card from cookie
-function removeCardFromCookie() {
-    document.cookie = "wishlistCard=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+// Function to remove card from cookie
+function removeCardFromCookie(card) {
+    var wishlistCards = getWishlistCardsFromCookie();
+    wishlistCards = wishlistCards.filter(function(storedCard) {
+        return storedCard !== card;
+    });
+    document.cookie = "wishlistCards=" + encodeURIComponent(JSON.stringify(wishlistCards)) + ";path=/";
 }
 
 // Function to retrieve budget amount from cookie
@@ -80,6 +108,23 @@ function getBudgetAmountFromCookie() {
         }
     }
     return 0;
+}
+
+// Function to retrieve wishlist cards from cookie
+function getWishlistCardsFromCookie() {
+    var name = "wishlistCards=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var cookieArray = decodedCookie.split(';');
+    for(var i = 0; i < cookieArray.length; i++) {
+        var cookie = cookieArray[i];
+        while (cookie.charAt(0) === ' ') {
+            cookie = cookie.substring(1);
+        }
+        if (cookie.indexOf(name) === 0) {
+            return JSON.parse(decodeURIComponent(cookie.substring(name.length)));
+        }
+    }
+    return [];
 }
 
 // Function to handle wishlist button click
